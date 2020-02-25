c4ea1c22bbd73585fe2f14877ec583c6c1b2053a
SoftwareSing <softwaresing@gmail.com>
2020-02-25 17:37:09 +0800

    Update for-ubuntu16.sh

--- for-ubuntu16.sh
+++ for-ubuntu16.sh
@@ -3,6 +3,7 @@
 # curl
 # git
 # lm-sensors
+# htop
 # nvm
 
 sudo apt install -y openssh-server
@@ -10,7 +11,7 @@ sudo service ssh restart
 
 sudo apt install -y curl git lm-sensors htop
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 echo "success install nvm"
 echo "use \"exec bash --login\" to reload terminal"
 echo "use \"nvm install --lts\" to install node"
